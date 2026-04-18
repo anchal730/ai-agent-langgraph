0ff48032819e0f8c63df6a8299c1996555fc433f
Anchal Bhatt <118708748+anchal730@users.noreply.github.com>
2026-04-18 23:51:44 +0530

    add streamlit secrets support

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ from langchain_community.utilities import WikipediaAPIWrapper
 # 🔐 Load Environment
 # =========================
 load_dotenv()
-groq_api_key = os.getenv("GROQ_API_KEY")
+# groq_api_key = os.getenv("GROQ_API_KEY")
+groq_api_key = os.getenv("GROQ_API_KEY") or st.secrets["GROQ_API_KEY"]
 
 if not groq_api_key:
     st.error("❌ GROQ_API_KEY not found in .env")
